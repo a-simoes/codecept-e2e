4b9113bbb864da343deb4a9bf224fd5cc037b3e6
André Simões <cwt00750@criticaltechworks.com>
2020-11-10 09:52:59 +0000

    Update docker-compose paths

--- codeceptjs/configuration/browserHelpers/webdriver.conf.js
+++ codeceptjs/configuration/browserHelpers/webdriver.conf.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const GeneralConfigurations = require('./../general.conf');
+const GeneralConfigurations = require('./../general.confs');
 
 module.exports = {
   url: GeneralConfigurations.siteUrl(),
